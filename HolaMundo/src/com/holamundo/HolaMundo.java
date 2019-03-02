4ed06b6fc45e6f86a6d63106823ad722940b8d66
rcm25 <rcm25@rcm93>
2019-03-02 14:15:54 +0100

    se ha agregado un comentario

--- HolaMundo/src/com/holamundo/HolaMundo.java
+++ HolaMundo/src/com/holamundo/HolaMundo.java
@@ -3,7 +3,7 @@ package com.holamundo;
 public class HolaMundo {
 
 	public static void main(String[] args) {
-
+		//comentario realizado desde nuevarama
 		System.out.println("Hola Mundo");
 		System.out.println("Hola Mundo2");
 	}
